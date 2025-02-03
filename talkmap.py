a61e70a57e725393ec16709967d4d99aa81b3c3d
jonathanlys01 <jonathanlystahiti@gmail.com>
2025-02-04 00:38:02 +0100

    Replace notebook with python script in github workflow

--- talkmap.py
+++ talkmap.py
@@ -11,39 +11,26 @@
 #
 # Requires: glob, getorg, geopy
 
-import glob
 
 import getorg
 from geopy import Nominatim
 
-
-g = glob.glob("*.md")
-
+# EDIT HERE
+places = [
+    "Brest, France", # MSc and PhD, Internship
+    "Paris, France", # Internship
+    "Stuttgart, Germany", # Visit
+    "Lyon, France", # Conference
+    "Sydney, Australia", # Academic exchange
+    "Papeete, French Polynesia", # Internship
+]
+# END EDIT
 
 geocoder = Nominatim()
-location_dict = {}
-location = ""
-permalink = ""
-title = ""
-
-
-for file in g:
-    with open(file, 'r') as f:
-        lines = f.read()
-        if lines.find('location: "') > 1:
-            loc_start = lines.find('location: "') + 11
-            lines_trim = lines[loc_start:]
-            loc_end = lines_trim.find('"')
-            location = lines_trim[:loc_end]
-
-
-        location_dict[location] = geocoder.geocode(location)
-        print(location, "\n", location_dict[location])
-
+location_dict = {place: geocoder.geocode(place) for place in places}
 
 m = getorg.orgmap.create_map_obj()
-getorg.orgmap.output_html_cluster_map(location_dict, folder_name="../talkmap", hashed_usernames=False)
-
+getorg.orgmap.output_html_cluster_map(location_dict, folder_name="../visitmap", hashed_usernames=False)
 
 
 
